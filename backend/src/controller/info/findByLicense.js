c37e6ec8757e84e9346e0376ae82b648beed553d
htq-uet <teoemga2003@gmail.com>
2023-04-01 15:41:26 +0700

    Update query

--- backend/src/controller/info/findByLicense.js
+++ backend/src/controller/info/findByLicense.js
@@ -2,7 +2,7 @@ import pool from "../../configs/connectDB"
 
 let findByLicense = async (req, res) => {
   let licenseId = req.body.licenseId
-  let base = `select * from vehicles v join `
+  let base = `select r.id AS r_id, r.name as r_name, v.*, `
 
   let type = `select type 
   from owner o 
@@ -12,16 +12,12 @@ let findByLicense = async (req, res) => {
   const [rows, fields] = await pool.query(type, [licenseId])
 
   if (rows[0].type === 1) {
-    let query = base + `personal p 
-    on v.ownerId = p.id
-    where v.licenseId = ?`
+    let query = base + ` p.* from vehicles v left join region r on v.regionId = r.id JOIN personal p on v.ownerId = p.id WHERE v.licenseId = ?`
     const [rows, fields] = await pool.query(query, [licenseId])
     return res.send({data: rows, ownerType: 1})
   }
   else {
-    let query = base + `company c 
-    on v.ownerId = c.id
-    where v.licenseId = ?`
+    let query = base + ` c.* from vehicles v left join region r on v.regionId = r.id JOIN company c on v.ownerId = c.id WHERE v.licenseId = ?`
     const [rows, fields] = await pool.query(query, [licenseId])
     return res.send({data: rows, ownerType: 0})
   }
