7be121c913865651ee6defec9369e5510a27c911
anhbanlinhle <vietlinhleviet@gmail.com>
2023-04-04 21:06:54 +0700

    data revolution

--- backend/sql_generate/sql/updatereg.sql
+++ backend/sql_generate/sql/updatereg.sql
@@ -1,4 +1,4 @@
 INSERT INTO `registry` (date, expire, centreId, licenseId)
-SELECT DATE_ADD(expire, INTERVAL 1 DAY), DATE_ADD(CURRENT_DATE(), INTERVAL 1 MONTH) + INTERVAL 1 DAY, centreId, licenseId
+SELECT DATE_ADD(expire, INTERVAL 1 DAY), DATE_ADD(expire, INTERVAL 18 MONTH) + INTERVAL 1 DAY, centreId, licenseId
 FROM registry
 WHERE expire < CURRENT_DATE();
