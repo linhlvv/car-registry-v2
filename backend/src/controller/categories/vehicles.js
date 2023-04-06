6d18a0861014ad5e6ca912d98edd2cdb56b76b41
anhbanlinhle <vietlinhleviet@gmail.com>
2023-04-06 11:28:00 +0700

    rewrite vehicles/all

--- backend/src/controller/categories/vehicles.js
+++ backend/src/controller/categories/vehicles.js
@@ -8,44 +8,52 @@ let vehicles = async (req, res) => {
   if (page === undefined)
     page = 1
 
-  let count = `select count(*) as total from vehicles`
-  const [countRows, countFields] = await pool.query(count)
-
-  let query = `
-  select v.licenseId, certDate, 
-  o.type, v.brand, v.model, v.version, p.name as owner,
-    case 
-      when expire >= CURRENT_DATE() then true
-        else false
-    end as valid
-      from vehicles v 
-    join region r 
-      on v.regionId = r.id 
-    join owner o 
-      on v.ownerId = o.id
-    join personal p
-      on p.id = o.id
-    left join registry re
-      on re.licenseId = v.licenseId
-          union all
-  select v.licenseId, certDate, 
-  o.type, v.brand, v.model, v.version, c.name as owner,
-    case
-      when expire >= CURRENT_DATE() then true
-        else false
-    end as valid
-      from vehicles v
-    join region r
-      on v.regionId = r.id
-    join owner o
-      on v.ownerId = o.id
-    join company c
-      on c.id = o.id
-    left join registry re
-      on re.licenseId = v.licenseId
-    order by licenseId
-    limit ? offset ?`
+  let count = `
+  select count(*) as total from registry
+  where (licenseId, expire) in
+  (select v.licenseId as license, max(expire) as expire
+  from vehicles v
+  left join registry re
+  on re.licenseId = v.licenseId
+  where centreId = ?
+  group by v.licenseId)`
+  const [countRows, countFields] = await pool.query(count, [req.session.userid])
   
+  let query = `
+  select re.licenseId as license, v.brand, v.model, v.version, re.date as registryDate, re.expire, p.name
+    from registry re
+  join vehicles v 
+    on v.licenseId = re.licenseId
+  join owner o 
+    on v.ownerId = o.id
+  join personal p
+    on p.id = o.id
+  where (re.licenseId, expire) in
+    (select v.licenseId as license, max(expire) as expire
+      from vehicles v
+    left join registry re
+      on re.licenseId = v.licenseId
+    where centreId = ` + req.session.userid +
+  `  group by re.licenseId)
+          union all 
+  select re.licenseId as license, v.brand, v.model, v.version, re.date as registryDate, re.expire, c.name
+    from registry re
+  join vehicles v 
+    on v.licenseId = re.licenseId
+  join owner o 
+    on v.ownerId = o.id
+  join company c 
+    on c.id = o.id
+  where (re.licenseId, expire) in
+    (select v.licenseId as license, max(expire) as expire
+      from vehicles v
+    left join registry re
+      on re.licenseId = v.licenseId
+    where centreId = ` + req.session.userid +
+  `  group by re.licenseId)
+    order by license
+    limit ? offset ?`
+
   const [rows, fields] = await pool.query(query, [resPerPage, 
                                                   resPerPage * (page - 1)])
   return res.send({data: rows, count: Math.ceil(countRows[0].total / resPerPage)})
