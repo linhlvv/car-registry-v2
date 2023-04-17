d4184d409a439e77df3964c7e5bfe5ee89071e4d
htq-uet <teoemga2003@gmail.com>
2023-04-17 13:27:52 +0700

    Update present on page

--- backend/src/controller/stats/allRegist.js
+++ backend/src/controller/stats/allRegist.js
@@ -14,9 +14,19 @@ let allRegist = async (req, res) => {
 
     let centreId = req.session.userid
     // query lấy về licenseId, Tên xe bao gồm brand, model, version ; regist date, expire date, và tên chủ sở hữu(tên công ty nếu type = 0, tên cá nhân nếu type = 1 )
-    let query = 'SELECT r.licenseId, brand, model, version, date, expire, p.name as name, (expire < CURRENT_DATE()) as status FROM registry r JOIN vehicles v on r.licenseId = v.licenseId JOIN personal p on v.ownerId = p.id \
+    let query = '\
+    SELECT r.licenseId, brand, model, version, date, expire, p.name as name, (expire < CURRENT_DATE())\
+    as status \
+    FROM registry r \
+    JOIN vehicles v on r.licenseId = v.licenseId \
+    JOIN personal p on v.ownerId = p.id \
     union all \
-    SELECT r.licenseId, brand, model, version, date, expire, c.name as name, (expire < CURRENT_DATE()) as status FROM registry r JOIN vehicles v on r.licenseId = v.licenseId JOIN company c on v.ownerId = c.id ORDER BY `licenseId` ASC\
+    SELECT r.licenseId, brand, model, version, date, expire, c.name as name, (expire < CURRENT_DATE()) \
+    as status \
+    FROM registry r \
+    JOIN vehicles v on r.licenseId = v.licenseId \
+    JOIN company c on v.ownerId = c.id \
+    ORDER BY `licenseId` ASC\
     limit ? offset ?'
     
 
