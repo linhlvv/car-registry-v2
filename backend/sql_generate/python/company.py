841ad0de9227a8c6b2a7874e475aa45cc72efcbd
anhbanlinhle <vietlinhleviet@gmail.com>
2023-04-24 09:17:15 +0700

    update database

--- backend/sql_generate/python/company.py
+++ backend/sql_generate/python/company.py
@@ -17,7 +17,7 @@ name_p1 = ["David", "Vodka", "Vladimir", "Viktor", "Hữu", "Văn", "Hữu", "Th
 name_p2 = ["Suy", "Quang", "Tuấn", "Ân", "Linh", "Quân", "Long", "Nam",
            "Phong", "Thắng", "Vũ", "Bình", "Đức", "Hoàng", "Minh", "Phúc", "Tú", "Vi"]
 
-for i in range(1110):
+for i in range(555):
     name = random.choice(heads) + " " + random.choice(mids) + " " + random.choice(
         acts) + " " + random.choice(name_p1) + " " + random.choice(name_p2)
     ran_names.append(name)
@@ -34,21 +34,21 @@ district = ["Cầu Giấy", "Đống Đa", "Ba Đình", "Hai Bà Trưng", "Hoàn
 province = ["Hà Nội", "Hồ Chí Minh", "Đà Nẵng", "Hải Phòng", "Cần Thơ", "An Giang", "Bà Rịa - Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước", "Bình Thuận", "Cà Mau", "Cao Bằng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Tĩnh", "Hải Dương", "Hậu Giang", "Hòa Bình", "Hưng Yên",
             "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái"]
 
-for i in range(1110):
+for i in range(555):
     ran_add = "số " + str(random.randrange(1, 200)) + ", " + random.choice(street) + ", " + \
         random.choice(ward) + ", " + random.choice(district) + \
         ", " + random.choice(province)
     ran_adds.append(ran_add)
 
 phones = []
-for i in range(1110):
+for i in range(555):
     phone = "09"
     for i in range(10):
         phone += str(random.randrange(0, 9))
     phones.append(phone)
 
 taxes = []
-for i in range(1110):
+for i in range(555):
     tax = ""
     for i in range(9):
         tax += str(random.randrange(0, 9))
