6a79690e28529d071f1fd12a0adca147f58ab5b9
vmtuan12 <21020664@vnu.edu.vn>
2023-04-27 17:13:22 +0700

    add duration column for expired cars

--- frontend/src/components/cars/RootRow.vue
+++ frontend/src/components/cars/RootRow.vue
@@ -18,8 +18,13 @@ const props = defineProps(['carType']);
                 Expired date
             </div>
         </div>
-        <div v-else class="font-bold text-[12px] max-[900px]:text-[11px] text-[#4f555a] flex items-center text-center justify-center w-[26%]">
-            Expired date
+        <div v-else class="w-[26%] flex items-center max-[736px]:hidden">
+            <div class="font-bold text-[12px] max-[900px]:text-[11px] text-[#4f555a] flex items-center text-center justify-center w-1/2">
+                Duration
+            </div>
+            <div class="font-bold text-[12px] max-[900px]:text-[11px] text-[#4f555a] flex items-center text-center justify-center w-1/2">
+                Expired date
+            </div>
         </div>
         <div class="font-bold text-[12px] max-[900px]:text-[11px] text-[#4f555a] flex items-center justify-end w-[17%]">
             Owner
