cfc829dff5a4056ccb91326abf2db2b4d98552a4
vmtuan12 <21020664@vnu.edu.vn>
2023-05-01 19:46:54 +0700

    add 2 modal

--- frontend/src/pages/Forecast.vue
+++ frontend/src/pages/Forecast.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Pagination from '../components/forecast/Pagination.vue';
 import ForecastCard from '../components/forecast/ForecastCard.vue';
+import CarInfoModal from '../components/modal/CarInfoModal.vue';
+import RegistrationFormModal from '../components/modal/RegistrationFormModal.vue';
 import { computed, onMounted, ref, watch } from 'vue';
 import { useAccountStore } from '../stores/AccountStore';
 
@@ -10,6 +12,22 @@ const accountStore = useAccountStore()
 const resPerPage = ref(7);
 
 const minHeight = ref('min-h-[400px]')
+
+//SECTION - modal
+const registModalOn = ref(false)
+const infoModalOn = ref(false)
+const modalLicense = ref()
+
+//FIXME - regist modal bug
+const handleRegistModal = (license) => {
+    modalLicense.value = license
+    registModalOn.value = !registModalOn.value
+}
+
+const handleInfoModal = (license) => {
+    modalLicense.value = license
+    infoModalOn.value = !infoModalOn.value
+}
 
 //SECTION - pagination
 const pageNumber = ref(1);
@@ -94,6 +112,12 @@ watch(() => resPerPage.value, () => {
 
 <template>
     <div>
+        <div v-if="registModalOn">
+            <RegistrationFormModal :license-id="modalLicense" @exit-modal="handleRegistModal"/>
+        </div>
+        <div v-if="infoModalOn">
+            <CarInfoModal :license-id="modalLicense" @exit-modal="handleInfoModal"/>
+        </div>
         <div class="my-6">
             <div class="flex justify-center">
                 <div class="flex items-center flex-col w-[90vw]">
@@ -115,7 +139,11 @@ watch(() => resPerPage.value, () => {
                     <div class="w-full flex flex-col gap-[2px] mb-4" :class="minHeight">
                         <ForecastCard :is-root-row="true"/>
                         <div v-for="card in forecastList" :key="card">
-                            <ForecastCard :info="card"/>
+                            <ForecastCard 
+                                :info="card"
+                                @open-info="handleInfoModal"
+                                @regist="handleRegistModal"
+                            />
                         </div>
                     </div>
                     <Pagination 
