88307892f9d9e3e63b4b267f9472cabca5afd5d8
vmtuan12 <21020664@vnu.edu.vn>
2023-05-03 01:17:39 +0700

    remove phone from info

--- frontend/src/components/account/AddAccountForm.vue
+++ frontend/src/components/account/AddAccountForm.vue
@@ -1,50 +1,89 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import Button from '../UI/Button.vue';
 import AccountInput from './AccountInput.vue';
 
 const accountInfo = ref({
     email: '',
-    phone: '',
     name: '',
     district: '',
     city: '',
 });
+
+const messageTime = 3
+const errorMessageOn = ref(false)
+const errorMessageTime = ref(messageTime)
+let errorMessageInterval;
+const handleAddAccount = () => {
+    if(accountInfo.value.email === '' ||
+        accountInfo.value.name === '' ||
+        accountInfo.value.district === '' ||
+        accountInfo.value.city === '') {
+            errorMessageOn.value = true
+            clearInterval(errorMessageInterval)
+            errorMessageTime.value = messageTime
+            errorMessageInterval = setInterval(() => {
+                errorMessageTime.value -= 1
+            }, 1000);
+    } else {
+
+    }
+};
+
+//SECTION - watchers
+watch(() => errorMessageTime.value, () => {
+    if(errorMessageTime.value === 0) {
+        errorMessageOn.value = false
+        errorMessageTime.value = messageTime
+    }
+})
+
 </script>
 
 <template>
-    <div class="flex flex-col gap-3 p-4 px-6 bg-white rounded-lg w-3/4 mt-2" :class="{'hidden': formClosed}">
-        <div class="flex gap-3 max-[960px]:flex-col w-full">
-            <div class="flex flex-col w-3/5 max-[960px]:w-full">
-                <AccountInput v-model="accountInfo.name" title="Name" placeholder="Name" type="text" class="w-full"/>
-            </div>
-            <div class="flex flex-col w-2/5 max-[960px]:w-full">
-                <AccountInput v-model="accountInfo.email" title="Email" placeholder="Email" type="email" class="w-full"/>
-            </div>
+    <div>{{ accountInfo }}</div>
+    <div class="flex flex-col gap-3 p-4 px-6 bg-white rounded-lg w-3/4 mt-2">
+        <div class="flex flex-col w-full">
+            <AccountInput v-model="accountInfo.name" title="Name" type="text" class="w-full"/>
         </div>
-        <div class="flex gap-3 w-full max-[960px]:flex-col">
-            <div class="flex items-center w-1/2 gap-3 max-[960px]:w-full">
-                <div class="flex flex-col w-[50%]">
-                    <AccountInput v-model="accountInfo.phone" title="Phone" placeholder="Phone" type="text" class="w-full"/>
-                </div>
-                <div class="flex flex-col w-[50%]">
-                    <AccountInput v-model="accountInfo.district" title="District" placeholder="District" type="text" class="w-full"/>
-                </div>
+        <div class="flex flex-col w-full">
+            <AccountInput v-model="accountInfo.email" title="Email" type="email" class="w-full"/>
+        </div>
+        <div class="flex flex-col w-full">
+            <AccountInput v-model="accountInfo.city" title="City" type="text" class="w-full"/>
+        </div>
+        <div class="flex flex-col w-full">
+            <AccountInput v-model="accountInfo.district" title="District" type="text" class="w-full"/>
+        </div>
+        <Transition name="slide-fade">
+            <div v-if="errorMessageOn" class="flex items-center gap-2 text-[#d54f16]">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                </svg>
+                <p class="text-base font-medium">Please fill all the fields</p>
             </div>
-            <div class="flex items-center w-1/2 gap-3 max-[960px]:w-full">
-                <div class="flex flex-col w-[50%]">
-                    <AccountInput v-model="accountInfo.city" title="City" placeholder="City" type="text" class="w-full"/>
-                </div>
-                <div class="w-[50%] flex items-end justify-end">
-                    <div class="flex text-[#1d1d1d] gap-1 p-2 rounded-lg items-center text-[13px] cursor-pointer duration-300 hover:text-[#16d592] active:bg-[#cdfcec]/40">
-                        <i class="fa-sharp fa-solid fa-circle-plus flex items-center"></i>
-                        <div class="font-semibold">CREATE ACCOUNT</div>
-                    </div>
-                </div>
-            </div>
+        </Transition>
+        <div class="w-full flex items-end justify-end py-2">
+            <button @click="handleAddAccount" class="flex text-[#1d1d1d] gap-1 rounded-lg items-center text-[13px] cursor-pointer duration-300 hover:text-[#16d592] active:bg-[#cdfcec]/40">
+                <i class="fa-sharp fa-solid fa-circle-plus flex items-center"></i>
+                <div class="font-semibold">CREATE ACCOUNT</div>
+            </button>
         </div>
     </div>
 </template>
 
 <style scoped>
+    .slide-fade-enter-active {
+		transition: all 0.5s ease-out;
+	}
+
+	.slide-fade-leave-active {
+		transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+	}
+
+	.slide-fade-enter-from,
+	.slide-fade-leave-to {
+		transform: translateX(50px);
+		opacity: 0;
+	}
 </style>
