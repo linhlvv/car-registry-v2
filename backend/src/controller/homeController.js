f7f23b4b2c19b415e3e4fadcbf8818f1e71ed6da
anhbanlinhle <vietlinhleviet@gmail.com>
2023-05-07 02:02:14 +0700

    sections for homeController

--- backend/src/controller/homeController.js
+++ backend/src/controller/homeController.js
@@ -1,23 +1,22 @@
 require("dotenv").config();
 
-import { authenticate } from "./user/authenticate/authenticate";
-import { logout } from "./user/authenticate/logout";
 import { verifyToken } from "./verifyToken";
 
-import { previewRegist } from "./centre/info/previewRegist";
-import { viewLatestRegist } from "./centre/info/viewLatestRegist";
-import { newRegist } from "./centre/management/newRegist";
-import { updateModify } from "./centre/management/updateModify";
+// section - user
+  // section - authenticate
+import { authenticate } from "./user/authenticate/authenticate";
+import { logout } from "./user/authenticate/logout";
+import { changePassword } from "./user/authenticate/changePassword";
+  // section - info
 import { centreInfo } from "./user/info/centreInfo";
 import { getDataForChart } from "./user/info/data4Chart";
 import { detailModal } from "./user/info/detailModal";
 import { previewInfo } from "./user/info/previewInfo";
+  // section - owner
+import { ownerInfo } from "./user/owner/ownerInfo";
 
-import { expired } from "./centre/vehicles/expired";
-import { findByLicense } from "./centre/vehicles/findByLicense";
-import { registed } from "./centre/vehicles/registed";
-import { vehicles } from "./centre/vehicles/vehicles";
-
+// section - centre
+  // section - filter
 import { allBrand } from "./centre/filter/allBrand";
 import { allCity } from "./centre/filter/allCity";
 import { brand } from "./centre/filter/brand";
@@ -25,23 +24,33 @@ import { exactBrand } from "./centre/filter/exactBrand";
 import { exactCity } from "./centre/filter/exactCity";
 import { owner } from "./centre/filter/owner";
 import { time } from "./centre/filter/time";
-
+  // section - forecast
+import { forecast } from "./centre/forecast/forecast";
+  // section - info
+import { previewRegist } from "./centre/info/previewRegist";
+import { viewLatestRegist } from "./centre/info/viewLatestRegist";
+  // section - management
+import { newRegist } from "./centre/management/newRegist";
+import { updateModify } from "./centre/management/updateModify";
+  // section - owner
 import { registHistory } from "./centre/owner/registHistory";
-import { ownerInfo } from "./user/owner/ownerInfo";
-
-import { changePassword } from "./user/authenticate/changePassword";
-
-import { addDataFromExcel } from "./department/management/addDataFromExcel";
-import { insertCentre } from "./department/management/insertCentre";
-import { removeCentre } from "./department/management/removeCentre";
-
+  // section - stats
 import { allRegist } from "./centre/stats/allRegist";
 import { registByLicense } from "./centre/stats/registByLicense";
 import { registByTime } from "./centre/stats/registByTime";
 import { registModal } from "./centre/stats/registModal";
+  // section - vehicles
+import { expired } from "./centre/vehicles/expired";
+import { findByLicense } from "./centre/vehicles/findByLicense";
+import { registed } from "./centre/vehicles/registed";
+import { vehicles } from "./centre/vehicles/vehicles";
 
-import { forecast } from "./centre/forecast/forecast";
-
+// section - department
+  // section - management
+import { addDataFromExcel } from "./department/management/addDataFromExcel";
+import { insertCentre } from "./department/management/insertCentre";
+import { removeCentre } from "./department/management/removeCentre";
+  // section - vehicles
 import { viewAllCentres } from "./department/vehicles/viewAllCentres";
 import { viewAllVehicles } from "./department/vehicles/viewAllVehicles";
 import { viewExpiredVehicles } from "./department/vehicles/viewExpiredVehicles";
