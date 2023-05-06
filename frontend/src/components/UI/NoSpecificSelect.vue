640cd273ca73aa772e8f4162c44300267aeaecc3
vmtuan12 <21020664@vnu.edu.vn>
2023-05-06 17:08:28 +0700

    no option selected interface

--- frontend/src/components/UI/NoSpecificSelect.vue
+++ frontend/src/components/UI/NoSpecificSelect.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="w-full h-full">
-        
+    <div class="w-full flex flex-col gap-5 items-center justify-center mt-10">
+        <i class="fa-solid fa-people-robbery text-9xl text-[#1d1d1d]"></i>
+        <p class="font-semibold text-2xl">Select an option to view!</p>
     </div>
 </template>
