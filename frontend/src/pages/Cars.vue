422668395b02fbba65d944aaf0cf5d711d6e2760
vmtuan12 <21020664@vnu.edu.vn>
2023-05-06 17:07:55 +0700

    [draft] add No option selected interface

--- frontend/src/pages/Cars.vue
+++ frontend/src/pages/Cars.vue
@@ -3,8 +3,11 @@ import CarInfoModal from '../components/modal/CarInfoModal.vue';
 import RegistrationFormModal from '../components/modal/RegistrationFormModal.vue';
 import CarList from '../components/cars/CarList.vue';
 import SearchField from '../components/cars/SearchField.vue';
-import BarChart from '../components/cars/BarChart.vue'
+import NoSpecificSelect from '../components/UI/NoSpecificSelect.vue';
+import { useAdminSelectionStore } from '../stores/AdminSelectionStore';
 import { reactive, ref } from 'vue';
+
+const adminSelectionStore = useAdminSelectionStore()
 
 //SECTION - page handler
 const pageNumber = ref(1);
@@ -157,100 +160,101 @@ const openSelectHandler = () => {
 
 <template>
     <div>
-        <!-- <div>{{ pageNumber }}</div> -->
-        <div v-if="carDetailModal">
-            <CarInfoModal :license-id="carInfoLicense" @exit-modal="turnOffModal"/>
-        </div>
-        <div v-if="registrationModal">
-            <RegistrationFormModal :license="carRegistLicense" @exit-modal="turnOffModal"/>
-        </div>
-
-        <!-- main -->
-        <div class="my-6">
-            <div class="flex items-center flex-col gap-5 justify-center">
-
-                <div class="">
-
+        <Transition name="fade" mode="out-in">
+            <div v-if="!adminSelectionStore.getVerified" class="w-full flex items-center">
+                <NoSpecificSelect />
+            </div>
+            <div v-else>
+                <!-- <div>{{ pageNumber }}</div> -->
+                <div v-if="carDetailModal">
+                    <CarInfoModal :license-id="carInfoLicense" @exit-modal="turnOffModal"/>
+                </div>
+                <div v-if="registrationModal">
+                    <RegistrationFormModal :license="carRegistLicense" @exit-modal="turnOffModal"/>
                 </div>
 
-                <!-- default car type filter -->
-                <div class="flex custom-shadow w-[70vw] rounded-2xl overflow-hidden bg-white max-[535px]:hidden">
-                    
-                    <div 
-                        @click="carTypeHandler('registed')"
-                        class="w-1/2 text-base font-medium gap-1 flex items-center justify-center text-center duration-200 cursor-pointer p-2 py-4 max-[997px]:text-sm max-[738px]:text-xs max-[625px]:text-[10px]"
-                        :class="carTypeMatched('registed') ? 'bg-[#2acc97] text-white' : 'hover:text-[#2acc97]'"
-                    >
-                        <i class="fa-solid fa-car-on"></i>
-                        <div>Registed cars</div>
-                    </div>
-                    <div 
-                        @click="carTypeHandler('expired')"
-                        class="w-1/2 text-base font-medium gap-1 flex items-center justify-center text-center duration-200 cursor-pointer p-2 py-4 max-[997px]:text-sm max-[738px]:text-xs max-[625px]:text-[10px]"
-                        :class="carTypeMatched('expired') ? 'bg-[#93a3e6] text-white' : 'hover:text-[#93a3e6]'"
-                    >
-                        <i class="fa-solid fa-car-burst"></i>
-                        <div>Expired cars</div>
-                    </div>
-                    
-                </div>
-                
-                <!-- responsive select dropdown -->
-                <div class="w-4/5 flex flex-col min-[535px]:hidden">
-                    <div @click="openSelectHandler" class="w-full rounded-lg p-2 flex items-center justify-between text-white text-base bg-[#2acc97]">
-                        <div class="font-semibold">Select</div>
-                        <i class="fa-solid fa-caret-down"></i>
-                    </div>
-                    <div :class="!selectionOpened ? 'hidden' : ''" class="w-full z-10 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-                        <ul class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"  >
-                            <div class="flex items-center">
-                                <input @click="carTypeHandler('registed')" id="default-radio-2" type="radio" value="registed" v-model="carType" name="registed" class="w-4 h-4">
-                                <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Registed cars</label>
+                <!-- main -->
+                <div class="my-6">
+                    <div class="flex items-center flex-col gap-5 justify-center">
+                        <!-- default car type filter -->
+                        <div class="flex custom-shadow w-[70vw] rounded-2xl overflow-hidden bg-white max-[535px]:hidden">
+                            <div 
+                                @click="carTypeHandler('registed')"
+                                class="w-1/2 text-base font-medium gap-1 flex items-center justify-center text-center duration-200 cursor-pointer p-2 py-4 max-[997px]:text-sm max-[738px]:text-xs max-[625px]:text-[10px]"
+                                :class="carTypeMatched('registed') ? 'bg-[#2acc97] text-white' : 'hover:text-[#2acc97]'"
+                            >
+                                <i class="fa-solid fa-car-on"></i>
+                                <div>Registed cars</div>
                             </div>
+                            <div 
+                                @click="carTypeHandler('expired')"
+                                class="w-1/2 text-base font-medium gap-1 flex items-center justify-center text-center duration-200 cursor-pointer p-2 py-4 max-[997px]:text-sm max-[738px]:text-xs max-[625px]:text-[10px]"
+                                :class="carTypeMatched('expired') ? 'bg-[#93a3e6] text-white' : 'hover:text-[#93a3e6]'"
+                            >
+                                <i class="fa-solid fa-car-burst"></i>
+                                <div>Expired cars</div>
+                            </div>
+                            
+                        </div>
                         
-                            <div class="flex items-center">
-                                <input @click="carTypeHandler('expired')" id="default-radio-3" type="radio" value="expired" v-model="carType" name="expired" class="w-4 h-4">
-                                <label for="default-radio-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Expired cars</label>
-                            </div>    
-                        </ul>
+                        <!-- responsive select dropdown -->
+                        <div class="w-4/5 flex flex-col min-[535px]:hidden">
+                            <div @click="openSelectHandler" class="w-full rounded-lg p-2 flex items-center justify-between text-white text-base bg-[#2acc97]">
+                                <div class="font-semibold">Select</div>
+                                <i class="fa-solid fa-caret-down"></i>
+                            </div>
+                            <div :class="!selectionOpened ? 'hidden' : ''" class="w-full z-10 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
+                                <ul class="p-3 space-y-3 text-sm text-gray-700 dark:text-gray-200"  >
+                                    <div class="flex items-center">
+                                        <input @click="carTypeHandler('registed')" id="default-radio-2" type="radio" value="registed" v-model="carType" name="registed" class="w-4 h-4">
+                                        <label for="default-radio-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Registed cars</label>
+                                    </div>
+                                
+                                    <div class="flex items-center">
+                                        <input @click="carTypeHandler('expired')" id="default-radio-3" type="radio" value="expired" v-model="carType" name="expired" class="w-4 h-4">
+                                        <label for="default-radio-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Expired cars</label>
+                                    </div>    
+                                </ul>
+                            </div>
+                        </div>
+                        
+                        <div class="flex custom-shadow items-center flex-col w-[90vw] rounded-2xl overflow-hidden">
+                            <SearchField
+                                @selectedFilterClicked="filterSelected" 
+                                @selectedCityClicked="citySelected"
+                                @selectedBrandClicked="brandSelected"
+                                @selectedTimeClicked="timeSelected"
+                                @selectedOwnerClicked="ownerEntered"
+                                @licenseSearch="licenseSearchEntered" 
+                                @next-page="nextPage" 
+                                @prev-page="prevPage" 
+                                @specified-page="specifiedPage"
+                                @sendSortOrder="bindOrder"
+                                @reloadData="reload"
+                                :page-num="pageNumber"
+                                :total-page="totalPage"
+                                :car-type="carType"
+                            />
+                            <CarList 
+                                :filter="filter" 
+                                :city="city"
+                                :brand="brand"
+                                :owner="owner"
+                                :time="time"
+                                :page-number="pageNumber" 
+                                :car-type="carType"
+                                :sort-order="order"
+                                :specific-license="licenseSearchContent"
+                                :reloaded="reloaded"
+                                @openCarInfo="openCarInfo" 
+                                @openCarRegistration="openCarRegistration"
+                                @totalPageNum="bindTotalPage"
+                            />
+                        </div>
                     </div>
-                </div>
-                
-                <div class="flex custom-shadow items-center flex-col w-[90vw] rounded-2xl overflow-hidden">
-                    <SearchField
-                        @selectedFilterClicked="filterSelected" 
-                        @selectedCityClicked="citySelected"
-                        @selectedBrandClicked="brandSelected"
-                        @selectedTimeClicked="timeSelected"
-                        @selectedOwnerClicked="ownerEntered"
-                        @licenseSearch="licenseSearchEntered" 
-                        @next-page="nextPage" 
-                        @prev-page="prevPage" 
-                        @specified-page="specifiedPage"
-                        @sendSortOrder="bindOrder"
-                        @reloadData="reload"
-                        :page-num="pageNumber"
-                        :total-page="totalPage"
-                        :car-type="carType"
-                    />
-                    <CarList 
-                        :filter="filter" 
-                        :city="city"
-                        :brand="brand"
-                        :owner="owner"
-                        :time="time"
-                        :page-number="pageNumber" 
-                        :car-type="carType"
-                        :sort-order="order"
-                        :specific-license="licenseSearchContent"
-                        :reloaded="reloaded"
-                        @openCarInfo="openCarInfo" 
-                        @openCarRegistration="openCarRegistration"
-                        @totalPageNum="bindTotalPage"
-                    />
                 </div>
             </div>
-        </div>
+        </Transition>
     </div>
 </template>
 
@@ -261,5 +265,15 @@ const openSelectHandler = () => {
 
     input[type=radio] {
         accent-color: #2acc97;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: all 0.3s ease;
+    }
+    
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
     }
 </style>
