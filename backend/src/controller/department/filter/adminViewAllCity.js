24f19db75f878cb06263864643c3610cbd182262
htq-uet <teoemga2003@gmail.com>
2023-05-12 15:16:00 +0700

    Done filter for admin

--- backend/src/controller/department/filter/adminViewAllCity.js
+++ backend/src/controller/department/filter/adminViewAllCity.js
@@ -13,7 +13,6 @@ let adminViewAllCity = async (req, res) => {
   from vehicles v join
   (select re.licenseId as license, max(expire) as expire
   from registry re
-  where centreId = ` + req.session.userid + `
   group by re.licenseId) ok
   on v.licenseId = ok.license
   join region r 
