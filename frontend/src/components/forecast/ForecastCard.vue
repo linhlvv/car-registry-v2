8c8605188ade2a8351d58b3579d75da9d773d1fb
vmtuan12 <21020664@vnu.edu.vn>
2023-05-14 21:37:19 +0700

    responsive forecast table

--- frontend/src/components/forecast/ForecastCard.vue
+++ frontend/src/components/forecast/ForecastCard.vue
@@ -21,7 +21,7 @@ const regist = (license) => {
 </script>
 
 <template>
-    <div class="w-full rounded-[3px] p-2 flex items-center min-h-[50px]" :class="isRootRow ? '' : 'bg-white'">
+    <div class="w-full rounded-[3px] p-2 flex items-center min-w-[800px] min-h-[50px]" :class="isRootRow ? '' : 'bg-white'">
         <p class="text-center font-medium text-[#1d1d1d] w-1/5" :class="isRootRow ? 'text-[14px]' : 'text-[12px]'">
             {{ isRootRow ? 'License ID' : info.licenseId }}
         </p>
@@ -32,14 +32,14 @@ const regist = (license) => {
             {{ isRootRow ? 'Expired date' : info.expire }}
         </p>
         <div v-if="!isRootRow" class="w-1/4 flex justify-evenly">
-            <div class="flex items-center justify-center gap-1 rounded-full text-[11px] bg-[#f5f7fb] px-3 py-1 mt-[2px] cursor-pointer duration-200 hover:bg-[#2acc97] text-[#293241] hover:text-white max-[820px]:bg-transparent max-[820px]:px-1" @click="openInfo(info.licenseId)">
-                <i class="fa-solid fa-circle-info flex items-center text-[12px] max-[952px]:hidden"></i>
+            <button class="flex items-center justify-center gap-1 rounded-full text-[11px] bg-[#f5f7fb] px-3 py-1 mt-[2px] cursor-pointer duration-200 hover:bg-[#2acc97] text-[#293241] hover:text-white" @click="openInfo(info.licenseId)">
+                <i class="fa-solid fa-circle-info flex items-center text-[12px]"></i>
                 <div class="font-semibold">Detail</div>
-            </div>
-            <div v-if="!accountStore.isAdmin" class="flex items-center justify-center gap-1 rounded-full text-[11px] bg-[#f5f7fb] px-3 py-1 mt-[2px] cursor-pointer duration-200 hover:bg-[#2acc97] text-[#293241] hover:text-white max-[820px]:bg-transparent max-[820px]:px-1" @click="regist(info.licenseId)">
-                <i class="fa-sharp fa-solid fa-registered flex items-center text-[12px] max-[952px]:hidden"></i>
+            </button>
+            <button v-if="!accountStore.isAdmin" class="flex items-center justify-center gap-1 rounded-full text-[11px] bg-[#f5f7fb] px-3 py-1 mt-[2px] cursor-pointer duration-200 hover:bg-[#2acc97] text-[#293241] hover:text-white" @click="regist(info.licenseId)">
+                <i class="fa-sharp fa-solid fa-registered flex items-center text-[12px]"></i>
                 <div class="font-semibold">Regist</div>
-            </div>
+            </button>
         </div>
     </div>
 </template>
