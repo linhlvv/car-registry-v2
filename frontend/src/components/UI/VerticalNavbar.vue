dcbf6029ec02659d07bfa3a6f6b1cb364c014260
vmtuan12 <21020664@vnu.edu.vn>
2023-05-15 19:54:38 +0700

    handle vertical navbar

--- frontend/src/components/UI/VerticalNavbar.vue
+++ frontend/src/components/UI/VerticalNavbar.vue
@@ -1,6 +1,7 @@
 <script setup>
 import VerticalNavbarButton from './VerticalNavbarButton.vue';
 const emit = defineEmits(['turnOffNav']);
+const props = defineProps(['navOn'])
 
 const turnOffNavHandler = () => {
     emit('turnOffNav')
@@ -8,30 +9,19 @@ const turnOffNavHandler = () => {
 </script>
 
 <template>
-    <div class="w-full h-full flex justify-end absolute z-50" @click="turnOffNavHandler">
-        <div class="w-2/5 h-full bg-[#2acc97] absolute p-3 flex flex-col gap-2 slide">
+    <div :class="navOn === false ? 'hidden' : ''" class="w-full h-screen top-0 flex justify-end absolute z-30" @click="turnOffNavHandler">
+        <div class="w-2/5 h-full bg-[#2acc97] rounded-l-2xl absolute pt-14 p-3 flex flex-col gap-2">
             <VerticalNavbarButton icon="fa-solid fa-user" content="Profile" link="/center/8"/>
             <VerticalNavbarButton icon="fa-solid fa-car" content="Cars" link="/cars"/>
             <VerticalNavbarButton icon="fa-sharp fa-solid fa-car-on" content="Registed cars" link="/registedCars"/>
             <VerticalNavbarButton icon="fa-sharp fa-solid fa-car-burst" content="Expired cars" link="/expiredCars"/>
             <VerticalNavbarButton icon="fa-solid fa-car-tunnel" content="Not registed cars" link="/notRegistedCars"/>
             <VerticalNavbarButton icon="fa-solid fa-lightbulb" content="Forecast" link="/forecast"/>
-            
         </div>
     </div>
 </template>
 
 <style scoped>
-    .slide {
-        animation: slide 0.5s;
-    }
-    @keyframes slide {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
-    }
+    
 </style>
 
