46a02b3c84e2479cf74ccd1377e94d5f2f2122b0
vmtuan12 <21020664@vnu.edu.vn>
2023-05-16 15:03:19 +0700

    adjust bg-color when selected

--- frontend/src/components/UI/VerticalNavbarButton.vue
+++ frontend/src/components/UI/VerticalNavbarButton.vue
@@ -6,7 +6,7 @@ const props = defineProps(['icon', 'content', 'link']);
 </script>
 
 <template>
-    <router-link :to="link" :class="route.path === link ? 'bg-white/50' : ''" class="w-full p-3 gap-4 rounded-md flex items-center text-white text-sm font-semibold hover:bg-white/50 cursor-pointer">
+    <router-link :to="link" :class="route.path === link ? 'bg-white/40' : ''" class="w-full p-3 gap-4 rounded-md flex items-center text-white text-sm font-semibold hover:bg-white/50 cursor-pointer">
         <i :class="icon"></i>
         <div class="">{{ content }}</div>
     </router-link>
