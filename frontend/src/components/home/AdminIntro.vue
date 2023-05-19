80506309724a57e08622202d0680a1f6a103b080
vmtuan12 <21020664@vnu.edu.vn>
2023-05-19 22:27:02 +0700

    add scroll to top button

--- frontend/src/components/home/AdminIntro.vue
+++ frontend/src/components/home/AdminIntro.vue
@@ -21,6 +21,10 @@ const scrollToServices = () => {
     document.documentElement.scrollTop = services.value.offsetTop
 };
 
+const scrollToTop = () => {
+    document.documentElement.scrollTop = 0
+};
+
 </script>
 
 <template>
@@ -131,6 +135,11 @@ const scrollToServices = () => {
                 <CategoryCard v-if="isAdmin" icon="fa-sharp fa-solid fa-users" category="Account management" link="/account-management" class="w-full"/> 
                 <CategoryCard v-if="isAdmin" icon="fa-sharp fa-solid fa-database" category="Manage database" link="/manage-database" class="w-full"/> 
             </div>
+        </div>
+        <div class="fixed z-10 bottom-4 right-4">
+            <svg @click="scrollToTop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 animate-bounce text-[#2acc97] cursor-pointer">
+                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm.53 5.47a.75.75 0 00-1.06 0l-3 3a.75.75 0 101.06 1.06l1.72-1.72v5.69a.75.75 0 001.5 0v-5.69l1.72 1.72a.75.75 0 101.06-1.06l-3-3z" clip-rule="evenodd" />
+            </svg>
         </div>
     </div>
 </template>
