805a51ad3d92e7186e5cf648f8a11c4b135c4b4c
vmtuan12 <21020664@vnu.edu.vn>
2023-05-19 21:06:34 +0700

    add services

--- frontend/src/components/home/AdminIntro.vue
+++ frontend/src/components/home/AdminIntro.vue
@@ -5,8 +5,8 @@ import CategoryCard from './CategoryCard.vue';
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col items-center relative overflow-hidden px-10">
-        <div class=" w-full flex items-center justify-end pt-5">
+    <div class="w-full h-full flex flex-col items-center relative overflow-hidden">
+        <div class=" w-full flex items-center justify-end pt-5 px-10">
             <div class="flex gap-5 text-lg text-white font-semibold">
                 <button class="relative inline-block after:contents-[''] after:w-full after:absolute after:scale-x-0 after:h-[2px] after:bottom-0 after:left-0 after:bg-white after:origin-bottom-right after:transition-transform after:duration-300 after:ease-in-out hover:after:scale-x-100 hover:after:origin-bottom-left">
                     About
@@ -19,7 +19,7 @@ import CategoryCard from './CategoryCard.vue';
                 </button>
             </div>
         </div>
-        <div class="w-full flex justify-around items-start ">
+        <div class="w-full flex justify-around items-start px-10">
             <div class="pt-40 text-white">
                 <p class="text-5xl font-bold mb-5">
                     Take your car registry management experience to the next level.
@@ -51,25 +51,27 @@ import CategoryCard from './CategoryCard.vue';
             <img :src="bg" alt="" class="w-[600px]"/>
         </div>
         <div class="w-[300%] absolute z-[-1] bottom-0 bg-[#93a3e6]/60 h-[200%] -translate-y-1/3 rotate-[6deg] rounded-bl-xl"></div>
-        <div class="w-full mt-24 py-10 px-5 rounded-lg bg-white border border-gray-100 hover:-translate-x-2 hover:-translate-y-2 hover:shadow-xl shadow transition-all duration-300 ease-in-out">
-            <p class="text-center text-2xl font-semibold mb-5"><span class="text-[#2acc97] text-4xl">A</span>bout</p>
-            <div class="font-medium text-sm text-justify">
-                <p class="font-semibold text-md ">
-                    The Vietnam Register (VR) is an organization directly under the Ministry of Transport, performing the function of state management of registration.
-                </p>
-                <p class=" ">
-                    ​Vietnam Register (VR) is an organization directly under the Ministry of Transport, performing the function of state management of registration of traffic vehicles and vehicles and equipment for ranking and testing. specialized tools, containers, boilers and pressure vessels used in road, railway, inland waterway and maritime transportation nationwide; organize the registration of technical safety and quality of all kinds of vehicles, transportation equipment and means of transport, equipment for oil and gas exploration, extraction and transportation at sea according to the provisions of law.
-                </p>
-                <p class=" ">
-                    The Vietnam Register has 24 Sub-Departments that carry out technical inspection of safety of watercraft and marine structures; there is a system of 140 centers/stations for technical inspection of road motor vehicle safety.
-                </p>
-                <p class=" ">
-                    The Vietnam Register has about 1,300 officials and employees, including more than 1000 technical staff with college and university degrees, about 100 staff with university degrees.
-                </p>
+        <div class="w-full px-10">
+            <div class="w-full mt-24 py-10 px-5 rounded-lg bg-white border border-gray-100 hover:-translate-x-2 hover:-translate-y-2 hover:shadow-xl shadow transition-all duration-300 ease-in-out">
+                <p class="text-center text-3xl font-semibold mb-5"><span class="text-[#2acc97] text-5xl">A</span>bout</p>
+                <div class="font-medium text-sm text-justify">
+                    <p class="font-semibold text-md ">
+                        The Vietnam Register (VR) is an organization directly under the Ministry of Transport, performing the function of state management of registration.
+                    </p>
+                    <p class=" ">
+                        ​Vietnam Register (VR) is an organization directly under the Ministry of Transport, performing the function of state management of registration of traffic vehicles and vehicles and equipment for ranking and testing. specialized tools, containers, boilers and pressure vessels used in road, railway, inland waterway and maritime transportation nationwide; organize the registration of technical safety and quality of all kinds of vehicles, transportation equipment and means of transport, equipment for oil and gas exploration, extraction and transportation at sea according to the provisions of law.
+                    </p>
+                    <p class=" ">
+                        The Vietnam Register has 24 Sub-Departments that carry out technical inspection of safety of watercraft and marine structures; there is a system of 140 centers/stations for technical inspection of road motor vehicle safety.
+                    </p>
+                    <p class=" ">
+                        The Vietnam Register has about 1,300 officials and employees, including more than 1000 technical staff with college and university degrees, about 100 staff with university degrees.
+                    </p>
+                </div>
             </div>
         </div>
-        <div class="w-full pt-24  pb-10">
-            <p class="text-center text-2xl font-semibold mb-8"><span class="text-[#2acc97] text-4xl">T</span>erms</p>
+        <div class="w-full pt-24 pb-10 px-10">
+            <p class="text-center text-3xl font-semibold mb-8"><span class="text-[#2acc97] text-5xl">T</span>erms</p>
             <div class="grid grid-cols-4 gap-4">
                 <div class="w-full flex flex-col bg-white items-center gap-4 py-10 shadow rounded-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.75" stroke="currentColor" class="w-8 h-8 p-1 bg-red-500/20 text-red-500 rounded-[50%]">
@@ -97,7 +99,7 @@ import CategoryCard from './CategoryCard.vue';
                 </div>
             </div>
         </div>
-        <div class="w-full grid grid-cols-2 items-center pt-24  pb-10 bg-[#2acc97]/10">
+        <div class="w-full grid grid-cols-2 items-center pt-24  pb-10 bg-[#2acc97]/10 px-10">
             <div class="text-center p-20">
                 <p class="text-3xl font-semibold text-left"><span class="text-[#2acc97] text-5xl">S</span>ervices</p>
                 <p class="text-lg font-medium text-left">With these functions, management will be easier than ever</p>
@@ -107,6 +109,8 @@ import CategoryCard from './CategoryCard.vue';
                 <CategoryCard icon="fa-solid fa-circle-plus" category="Regist" link="/regist-new-car" class="w-full"/>
                 <CategoryCard icon="fa-solid fa-lightbulb" category="Forecast" link="/forecast" class="w-full"/> 
                 <CategoryCard icon="fa-solid fa-chart-simple" category="Statistics" link="/statistics" class="w-full"/> 
+                <CategoryCard icon="fa-sharp fa-solid fa-users" category="Account management" link="/account-management" class="w-full"/> 
+                <CategoryCard icon="fa-sharp fa-solid fa-database" category="Manage database" link="/manage-database" class="w-full"/> 
             </div>
         </div>
     </div>
