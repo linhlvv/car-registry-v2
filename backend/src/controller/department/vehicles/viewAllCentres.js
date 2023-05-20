0a7665eec818ccb2a892f23c029187145882f46d
htq-uet <teoemga2003@gmail.com>
2023-05-20 09:36:01 +0700

    +Preview email of all centre

--- backend/src/controller/department/vehicles/viewAllCentres.js
+++ backend/src/controller/department/vehicles/viewAllCentres.js
@@ -6,7 +6,7 @@ let viewAllCentres = async (req, res) => {
 	if (req.session.authority !== 1) 
 		return res.sendStatus(403)
 
-	let query = 'select * from centre'
+	let query = 'select c.*, a.email from centre c natural join account a'
 	const [rows, fields] = await pool.query(query)
 	return res.send({data: rows})
 }
