9173ba70670de5005b44d891a9df1d13f1283384
vmtuan12 <21020664@vnu.edu.vn>
2023-05-20 22:01:21 +0700

    fix error message

--- frontend/src/pages/ManageDatabase.vue
+++ frontend/src/pages/ManageDatabase.vue
@@ -113,7 +113,7 @@ watch(() => ownerTypePicked.value, (newType, oldType) => {
 				<p class="font-medium text-base text-[#1d1d1d]">{{ selectedFile !== null ? selectedFile.name : 'No chosen file' }}</p>
 			</div>
 			<div v-if="hasFile">
-				<svg @click="removeFile" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 text-[#cc5b2a] p-3 rounded-[50%] cursor-pointer hover:bg-[#cc5b2a]/20">
+				<svg @click="removeFile" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 text-[#cc5b2a] p-3 rounded-[50%] cursor-pointer ease-in-out duration-200 hover:bg-[#cc5b2a]/10 active:bg-[#cc5b2a]/20">
 					<path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
 				</svg>
 			</div>
@@ -143,7 +143,7 @@ watch(() => ownerTypePicked.value, (newType, oldType) => {
 			</div>
 		</Transition>
 		<Transition name="fade">
-			<div v-if="ownerTypeErrorMessageOn" class="w-full flex items-center p-3 gap-2 text-[#cc5b2a]">
+			<div v-if="ownerTypeErrorMessageOn && hasFile" class="w-full flex items-center p-3 gap-2 text-[#cc5b2a]">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
 					<path fill-rule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
 				</svg>
