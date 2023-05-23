b618d7ac4aefe77035d64a9d9c991de00fc9dcf6
vmtuan12 <21020664@vnu.edu.vn>
2023-05-23 15:11:18 +0700

    add animation to navbar button

--- frontend/src/components/UI/NavbarButton.vue
+++ frontend/src/components/UI/NavbarButton.vue
@@ -4,9 +4,19 @@ const props = defineProps(['icon', 'link', 'content', 'current']);
 </script>
 
 <template>
-    <router-link :to="link" :class="{underlined: current}" class= "text-[#293241] border-2 border-solid border-transparent h-full text-center dark:text-white duration-300 hover:text-base hover:bg-[#2acc97] hover:text-white px-4 py-3 flex items-center gap-1">
-        <i :class="icon"></i>
-        {{ content }}
+    <router-link :to="link" :class="{underlined: current}" class= "text-[#293241] group relative overflow-hidden border-2 border-solid border-transparent h-full text-center dark:text-white duration-300 hover:bg-[#2acc97] hover:text-white px-4 py-3 flex flex-col">
+        <div class="absolute flex items-center space-x-2 duration-300 transition-transform -translate-y-10 group-hover:block group-hover:-translate-y-0">
+            <i 
+                :class="icon"
+            ></i>
+            <span>{{ content }}</span>
+        </div>
+        <div class="flex items-center space-x-2 duration-300 transition-transform group-hover:translate-y-10">
+            <i 
+                :class="icon"
+            ></i>
+            <span>{{ content }}</span>
+        </div>
     </router-link>
 </template>
 
