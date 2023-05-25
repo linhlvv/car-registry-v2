427661184c02923cc8e0d86a590f400abd3185a9
vmtuan12 <21020664@vnu.edu.vn>
2023-05-26 01:01:54 +0700

    conditional render for type classification

--- frontend/src/pages/Forecast.vue
+++ frontend/src/pages/Forecast.vue
@@ -193,16 +193,16 @@ watch(() => adminSelectionStore.getSelected, () => {
                                     <p class="text-[#2acc97]">{{ totalRes }}</p>
                                 </div>
                                 <div class="flex items-center space-x-6">
-                                    <div class="flex items-center shadow-sm rounded-md overflow-hidden bg-white">
+                                    <div v-if="isAdmin" class="flex items-center shadow-sm rounded-md overflow-hidden bg-white">
                                         <div @click="changeStatus('renew')" class="flex items-center p-3 cursor-pointer duration-300 ease-in-out transition-all relative overflow-hidden" :class="status === 'renew' ? 'bg-red-500 text-white w-28' : 'text-red-500 w-12'">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
                                             </svg>
-                                            <span :class="status === 'renew' ? '' : 'hidden'" class="font-semibold ml-9 text-sm absolute">Expired</span>
+                                            <span :class="status === 'renew' ? '' : ''" class="font-semibold ml-9 text-sm absolute">Expired</span>
                                         </div>
-                                        <div @click="changeStatus('new')" class="flex items-center p-3 cursor-pointer duration-300 ease-in-out relative overflow-hidden" :class="status === 'new' ? 'bg-[#93a3e6] text-white w-[85px]' : 'text-[#93a3e6] w-12'">
-                                            <p :class="status === 'new' ? '' : 'hidden'" class="font-semibold text-sm absolute">New</p>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" :class="status === 'new' ? 'ml-9' : ''" >
+                                        <div @click="changeStatus('new')" class="flex justify-end items-center p-3 cursor-pointer duration-300 ease-in-out relative overflow-hidden" :class="status === 'new' ? 'bg-[#93a3e6] text-white w-24' : 'text-[#93a3e6] w-12'">
+                                            <p :class="status === 'new' ? '' : ''" class="font-semibold text-sm mr-9 absolute">New</p>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
                                             </svg>
                                         </div>
