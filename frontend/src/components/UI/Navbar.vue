e8a0c525c3cb31091e95d9539c29d58138226b8c
vmtuan12 <21020664@vnu.edu.vn>
2023-05-28 00:41:23 +0700

    adjust padding

--- frontend/src/components/UI/Navbar.vue
+++ frontend/src/components/UI/Navbar.vue
@@ -189,7 +189,7 @@ onMounted(() => {
             <nav class="bg-white">
                 <div class="max-w-screen-xl mx-auto">
                     <div class="flex items-center justify-between">
-                        <button @click="moveSelection('left')" class="flex justify-center items-center cursor-pointer w-[10%] py-6 text-[#2acc97] duration-200 hover:text-white hover:bg-[#2acc97]/90 active:bg-[#2acc97]">
+                        <button @click="moveSelection('left')" class="flex justify-center items-center cursor-pointer w-[10%] py-8 text-[#2acc97] duration-200 hover:text-white hover:bg-[#2acc97]/90 active:bg-[#2acc97]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
@@ -210,7 +210,7 @@ onMounted(() => {
                             </div>
                         </Transition>
                         
-                        <button @click="moveSelection('right')" class="flex justify-center items-center cursor-pointer w-[10%] py-6 text-[#2acc97] duration-200 hover:text-white hover:bg-[#2acc97]/90 active:bg-[#2acc97]">
+                        <button @click="moveSelection('right')" class="flex justify-center items-center cursor-pointer w-[10%] py-8 text-[#2acc97] duration-200 hover:text-white hover:bg-[#2acc97]/90 active:bg-[#2acc97]">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
