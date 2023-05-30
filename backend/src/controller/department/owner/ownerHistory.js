a8b050258d329474a3ba072101f6b39bd7f08f55
anhbanlinhle <vietlinhleviet@gmail.com>
2023-05-31 01:16:13 +0700

    fix history logic

--- backend/src/controller/department/owner/ownerHistory.js
+++ backend/src/controller/department/owner/ownerHistory.js
@@ -8,98 +8,57 @@ let ownerHistory = async (req, res) => {
   }
   let type = ""
   let queryType = ""
-
-  let carType = "registed"
+  let carType = ""
+  let query = ""
   let convert = () => {
     type = carType === "expired" ? " < " : " >= ";
     queryType = carType === "registed"
         ? "re.date as registryDate"
         : "timestampdiff(month, re.date, re.expire) as duration"
+    query = `
+      select re.licenseId as license, v.brand, v.model, v.version, ` +
+        queryType + `, re.expire, p.name, ce.name
+        from registry re
+      join vehicles v 
+        on v.licenseId = re.licenseId
+      join owner o 
+        on v.ownerId = o.id
+      join personal p
+        on p.id = o.id
+      join centre ce
+        on ce.id = re.centreId
+      where ownerId = ?
+        and expire` + type + `current_date()
+              union all 
+      select re.licenseId as license, v.brand, v.model, v.version, ` +
+        queryType + `, re.expire, c.name, ce.name
+        from registry re
+      join vehicles v 
+        on v.licenseId = re.licenseId
+      join owner o 
+        on v.ownerId = o.id
+      join company c 
+        on c.id = o.id
+      join centre ce
+        on ce.id = re.centreId
+      where ownerId = ?
+        and expire` + type + `current_date()`
   }
-  console.log(ownerid);
 
-  convert()
-  let query = `
-  select re.licenseId as license, v.brand, v.model, v.version, ` +
-    queryType +
-    `, re.expire, p.name, ce.name
-    from registry re
-  join vehicles v 
-    on v.licenseId = re.licenseId
-  join owner o 
-    on v.ownerId = o.id
-  join personal p
-    on p.id = o.id
-  join centre ce
-    on ce.id = re.centreId
-  where (re.licenseId, expire) in
-    (select v.licenseId as license, max(expire) as expire
-      from vehicles v
-    left join registry re
-      on re.licenseId = v.licenseId
-    where ownerId = ?
-      group by re.licenseId)
-  and expire` + type + `current_date()
-          union all 
-  select re.licenseId as license, v.brand, v.model, v.version, ` +
-    queryType +
-    `, re.expire, c.name, ce.name
-    from registry re
-  join vehicles v 
-    on v.licenseId = re.licenseId
-  join owner o 
-    on v.ownerId = o.id
-  join company c 
-    on c.id = o.id
-  join centre ce
-    on ce.id = re.centreId
-  where (re.licenseId, expire) in
-    (select v.licenseId as license, max(expire) as expire
-      from vehicles v
-    left join registry re
-      on re.licenseId = v.licenseId
-    where ownerId = ?
-      group by re.licenseId)
-  and expire` + type + `current_date()`
+  try {
+    carType = "registed"
+    convert()
+    const [registRows, fields1] = await pool.query(query, [ownerid, ownerid])
 
-  console.log(query);
-  const [registRows, fields1] = await pool.query(query, [ownerid, ownerid])
-
-  carType = "expired"
-  convert()
-  query = `
-  select re.licenseId as license, v.brand, v.model, v.version, ` +
-    queryType +
-    `, re.expire, p.name, ce.name
-    from registry re
-  join vehicles v 
-    on v.licenseId = re.licenseId
-  join owner o 
-    on v.ownerId = o.id
-  join personal p
-    on p.id = o.id
-  join centre ce
-    on ce.id = re.centreId
-    where ownerId = ?
-  and expire` + type + `current_date()
-          union all 
-  select re.licenseId as license, v.brand, v.model, v.version, ` +
-    queryType +
-    `, re.expire, c.name, ce.name
-    from registry re
-  join vehicles v 
-    on v.licenseId = re.licenseId
-  join owner o 
-    on v.ownerId = o.id
-  join company c 
-    on c.id = o.id
-  join centre ce
-    on ce.id = re.centreId
-    where ownerId = ?
-  and expire` + type + `current_date()`
-
-  const [expireRows, fields2] = await pool.query(query, [ownerid, ownerid])
-  return res.send({ registed: registRows, expired: expireRows })
+    carType = "expired"
+    convert()
+    const [expireRows, fields2] = await pool.query(query, [ownerid, ownerid])
+    return res.send({ registed: registRows, expired: expireRows })
+  }
+  catch (err) {
+    console.log(err)
+    return res.status(500).send({ErrorCode: err.code, ErrorNo: err.errno})
+  }
 }
 
 module.exports = {
