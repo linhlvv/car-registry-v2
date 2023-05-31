37df26b0f69c77a2c3af235a696a4c960a1bf51c
vmtuan12 <21020664@vnu.edu.vn>
2023-05-31 15:15:26 +0700

    hotfix owner profile

--- frontend/src/components/owner/OwnerRegistryHistory.vue
+++ frontend/src/components/owner/OwnerRegistryHistory.vue
@@ -60,13 +60,13 @@ onMounted(() => {
         <div id="table" class="flex flex-col w-full gap-[2px] overflow-x-scroll">
             <OwnerRegistryCard :is-root-row="true" license-plate="License plate" name="Name"/>
             <div v-if="registedTag && registedList.length > 0" v-for="card in registedList" :key="card.licensePlate" class="w-full">
-                <OwnerRegistryCard :is-root-row="false" :license-plate="card.license" :name="`${card.brand} ${card.model} ${card.version}`" :regist-date="card.registryDate" :expired-date="card.expire" :center="card.name"/>
+                <OwnerRegistryCard type="1" :is-root-row="false" :license-plate="card.license" :name="`${card.brand} ${card.model} ${card.version}`" :regist-date="card.registryDate" :expired-date="card.expire" :center="card.name"/>
             </div>
             <div v-else-if="registedTag && registedList.length === 0" class="slide-fade p-2 text-[#f5604c] text-base font-semibold w-full items-center justify-center text-center">
                 No valid car available
             </div>
             <div v-if="!registedTag && expiredList.length > 0" v-for="card in expiredList" :key="card.licensePlate" class="w-full">
-                <OwnerRegistryCard :is-root-row="false" :license-plate="card.license" :name="`${card.brand} ${card.model} ${card.version}`" :regist-date="card.registryDate" :expired-date="card.expire" :center="card.name"/>
+                <OwnerRegistryCard type="0" :is-root-row="false" :license-plate="card.license" :name="`${card.brand} ${card.model} ${card.version}`" :regist-date="card.registryDate" :expired-date="card.expire" :center="card.name" :duration="card.duration"/>
             </div>
             <div v-else-if="!registedTag && expiredList.length === 0" class="slide-fade p-2 text-[#f5604c] text-base font-semibold w-full items-center justify-center text-center">
                 No invalid car available
