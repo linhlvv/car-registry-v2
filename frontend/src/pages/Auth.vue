99bdc2ff3ebd77f1a116b13bc8a33e96ef54adee
anhbanlinhle <vietlinhleviet@gmail.com>
2024-12-29 22:16:10 +0700

    feat: remove frontend

--- frontend/src/pages/Auth.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="h-screen w-full flex flex-col justify-center items-end lg:bg-[url('../assets/loginBg.png')] lg:bg-contain lg:bg-no-repeat lg:bg-left">
-        <router-view />
-    </div>
-</template>
-
-<style scoped>
-</style>
